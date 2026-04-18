7db11e358b5074040999fe048beffe6ac3d1f957
TryYourBestAndLeaveTheRest <sherifdeenraji96@gmail.com>
2026-04-18 22:53:37 +0100

    fix: set default PORT value to 8080 in server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT,  '0.0.0.0', () => {
     console.log(`Campus Swap & Drop modular server running at http://localhost:${PORT}`);
